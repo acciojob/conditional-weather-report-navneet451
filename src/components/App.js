27d91f63c715cc15cef9ba13d3a0e1946b8cdb38
Navneet_thakur <127828104+navneet451@users.noreply.github.com>
2024-11-19 11:53:53 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,21 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState, useEffect } from "react";
+import WeatherDisplay from "./WeatherDisplay";
 
 const App = () => {
+  const [weather, setWeather] = useState({ temperature: 0, conditions: "" });
+
+  useEffect(() => {
+    // Simulating the given weather input
+    const weatherInput = { temperature: 25, conditions: "Sunny" };
+    setWeather(weatherInput);
+  }, []);
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <WeatherDisplay weather={weather} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
